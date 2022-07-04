13f90def6c2d4e40e91d55d8dc8b8dfab180b6ed
Myriam <myriam.rivera.santiago@gmail.com>
2022-07-04 11:02:17 -0500

    se hacen pruebas

--- src/view/login.js
+++ src/view/login.js
@@ -1,49 +1,52 @@
 export default () => {
   const viewLoginTemplate = `
-        <div class ="divImgLogin">
-        <img src ="">
-        </div>
 
-    <div class="divFormLogin"> 
 
-        <div class="form-container">
-        <figure>
-        <img scr="../imagenes/pawn.png" alt="imagen">
-        </figure>
 
-        <h3>Login</h3>
+      <div class ="divImgLogin">
+          <img src ="">
+      </div>
 
-        <form class='formLogin'>
-            <div class="group">
-            <label class="labelLogin" for = "idEmailLogin">E-mail</label>
-            </div>
-            <div class="group">
-            <input id="idEmailLogin" type="email">
-            </div>
+      <div class="divFormLogin"> 
 
-            <div class="group">
-            <label class="labelLogin" for = "idPasswordLogin">Password</label>
-            </div>
-            <div class="group">
-            <input id="idPasswordLogin" type="password">
-            </div>
+      <div class="form-container">
+          <figure>
+          <img src = "./imagenes/pawn.png" alt="imagen">
+          </figure>
 
-            <div class="buttonClass">
-            <button class="buttons" id="idButtonLogin" type="submit">Inciar sesión</button>
-            </div>
-        </form> 
+          <h3>Login</h3>
 
-        <div class="socialmedia">
-          <h4>o inicia sesión con</h4>
-        
-          <div class="socialmediaOptions">
-          <ul>
-          <li><img src="" alt="ícono Facebook"></li>
-          <li><img src="" alt="ícono Google"></li>
-          </ul>
+          <form class='formLogin'>
+              <div class="group">
+              <label class="labelLogin" for = "idEmailLogin">E-mail</label>
+              </div>
+              <div class="group">
+              <input id="idEmailLogin" type="email">
+              </div>
+
+              <div class="group">
+              <label class="labelLogin" for = "idPasswordLogin">Password</label>
+              </div>
+              <div class="group">
+              <input id="idPasswordLogin" type="password">
+              </div>
+
+              <div class="buttonClass">
+              <button class="buttons" id="idButtonLogin" type="submit">Inciar sesión</button>
+              </div>
+          </form> 
+
+          <div class="socialmedia">
+            <h4>o inicia sesión con</h4>
+          
+            <div class="socialmediaOptions">
+            <ul>
+            <li><img src="" alt="ícono Facebook"></li>
+            <li><img src="" alt="ícono Google"></li>
+            </ul>
+          </div> 
         </div> 
-      </div> 
-    </div>
+      </div>
 
       
   `;
