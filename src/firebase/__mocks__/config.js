c98681bbab43fc9abc1c17a89ada747dfa6dd17a
elisabethfarfan <elisabethfarfan@gmail.com>
2022-07-22 17:26:22 -0500

    mocks

--- src/firebase/__mocks__/config.js
+++ src/firebase/__mocks__/config.js
@@ -1,3 +1,8 @@
-export const createUserWithEmailAndPassword = jest.fn(() => Promise.resolve({}))
+export const createUserWithEmailAndPassword = jest.fn((auth,email,password) => Promise.resolve({email:'élcorreo@em,ail.com', nombre:"ël nombre"}))
+// Promise.reject({message:"no se puedo guaradar" })
 
+// new Promise((resolve,reject)=>{
+// if(!email) reject("email invalido")
+// resolve({user:'élcorreo'})
+// })
 //  .reject para catch  document.html.innerHTML = "<div></div>"
