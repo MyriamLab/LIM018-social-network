2911964cb86ce238d3c54937893b6a89b002a4b5
Myriam <myriam.rivera.santiago@gmail.com>
2022-07-25 11:12:08 -0500

    actualización

--- src/firebase/__mocks__/config.js
+++ src/firebase/__mocks__/config.js
@@ -1,4 +1,4 @@
-export const createUserWithEmailAndPassword = jest.fn((auth,email,password) => Promise.resolve({email:'élcorreo@em,ail.com', nombre:"ël nombre"}))
+export const createUserWithEmailAndPassword = jest.fn((auth,email,password) => Promise.resolve({email:'elcorreo@email.com', nombre:"el nombre"}))
 // Promise.reject({message:"no se puedo guaradar" })
 
 // new Promise((resolve,reject)=>{
